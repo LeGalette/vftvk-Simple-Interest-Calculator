551f240f7d79868bf67cb5ef272740bee38fc85e
Sascha Rezagholinia <sascha.rezagholinia@sennder.com>
2021-10-10 17:31:31 +0200

    finishing the lab

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    today = new Date();
+	finalYear = parseInt(today.getFullYear()) + parseInt(years);
+
+    if (principal > 0) {
+    document.getElementById("result").innerHTML = 
+    "<br><br>If you deposit:<font class='deposit'>"+principal+"</font><br>With a rate of:<font class='result'>"+rate+"%</font>,<br>Your interest will be:<font class='result'>"+interest+"</font>,<br>And your last year will be:<font class='result'>"+finalYear+"</font>";
+    } else{
+        document.getElementById("result").innerHTML = "<br><font class='negative'>please enter a positive number</br>"
+    }
     
 }
-        
+     
+function updateRate() 
+{
+    var rate = document.getElementById("rate").value;
+    console.log(rate)
+    document.getElementById("rate_value").innerText=rate;
+}
